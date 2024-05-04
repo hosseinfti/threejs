de3ea7a1b90b5912e80ed62c313c9bd2093af4c5
h.fathi <hosseinfti@gmail.com>
2024-05-04 23:21:11 +0330

    Added: window size hook

--- threejs-test/src/App.tsx
+++ threejs-test/src/App.tsx
@@ -1,50 +1,72 @@
-import './App.css'
-import {Scene, SphereGeometry, MeshBasicMaterial, Mesh, PerspectiveCamera, WebGLRenderer, PointLight} from "three"
-import {useEffect, useRef} from "react";
-
+import "./App.css";
+import {
+  Scene,
+  SphereGeometry,
+  MeshBasicMaterial,
+  Mesh,
+  PerspectiveCamera,
+  WebGLRenderer,
+  PointLight,
+} from "three";
+import { useEffect, useRef } from "react";
+import useWindowSize from "./utils/useWindowSize";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 function App() {
+  const sceneRef = useRef<HTMLCanvasElement | null>(null);
 
-    const sceneRef = useRef<HTMLCanvasElement | null>(null)
+  const windowSize = useWindowSize();
 
-    useEffect(() => {
-        if (sceneRef?.current) {
+  useEffect(() => {
+    if (sceneRef?.current) {
+      const sizes = {
+        width: windowSize.width,
+        height: windowSize.height,
+      };
+      //scene
+      const scene = new Scene();
+      const geometry = new SphereGeometry(3, 64, 64);
+      const material = new MeshBasicMaterial({
+        color: "#00ff83",
+      });
 
-            //scene
-            const scene = new Scene();
-            const geometry = new SphereGeometry(3, 64, 64)
-            const material = new MeshBasicMaterial({
-                color: "#00ff83",
-            })
+      //mesh
+      const mesh = new Mesh(geometry, material);
+      scene.add(mesh);
 
-            //mesh
-            const mesh = new Mesh(geometry, material)
-            scene.add(mesh)
+      //light
+      const light = new PointLight(0xff0000);
+      light.position.set(10, 10, 10);
+      scene.add(light);
 
-            //light
-            const light = new PointLight(0xff0000, 1, 100)
-            light.position.set(50, 50, 50)
-            scene.add(light)
+      //camera
+      const camera = new PerspectiveCamera(
+        25,
+        sizes.width / sizes.height,
+        10,
+        100
+      );
+      camera.position.z = 51;
+      scene.add(camera);
 
-            //camera
-            const camera = new PerspectiveCamera(45, 800 / 600, 1, 1000)
-            camera.position.z = 20
-            scene.add(camera)
+      //render
+      let canvas = sceneRef.current;
+      const renderer = new WebGLRenderer({ canvas });
+      renderer.setSize(sizes.width, sizes.height);
+      renderer.render(scene, camera);
 
-            //render
-            let canvas = sceneRef.current
-            const renderer = new WebGLRenderer({canvas})
-            renderer.setSize(800, 600)
-            renderer.render(scene, camera)
+      const controls = new OrbitControls(camera, canvas);
+      //   mesh.position.x += 100;
+      //   camera.updateProjectionMatrix();
+    }
+  }, [sceneRef, windowSize]);
 
-        }
+  //   useEffect(() => {
+  //     //resize
 
-    }, [sceneRef])
+  //   }, [windowSize]);
 
-
-    return (
-        <canvas ref={sceneRef}></canvas>
-    )
+  return <canvas ref={sceneRef}></canvas>;
 }
 
-export default App
+export default App;
---
+++ threejs-test/src/utils/useWindowSize.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+interface WindowSize {
+  width: number;
+  height: number;
+}
+
+function useWindowSize(): WindowSize {
+  const [windowSize, setWindowSize] = useState<WindowSize>({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup function to remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []); // Empty array ensures effect is only run on mount and unmount
+
+  return windowSize;
+}
+
+export default useWindowSize;
